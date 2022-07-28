401a8c860dac7150269f71d7d47917b61fce6631
nguyendacdung1 <nguyendungmk9@gmail.com>
2022-07-28 08:56:58 +0700

    employee

---
+++ employeeCRUD/config.php
@@ -0,0 +1,15 @@
+<?php
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'demo');
+
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+//check connect
+
+if ($link === false){
+    die("ERROR: Cound not connect. " . mysqli_connect_error());
+}
+?>
